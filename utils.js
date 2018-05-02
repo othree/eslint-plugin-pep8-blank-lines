ab2d59ef1195b1e6e2bfd43b18a372ad654d7419
othree <othree@gmail.com>
2018-05-02 18:21:15 +0800

    Top level delcaration

--- utils.js
+++ utils.js
@@ -6,6 +6,17 @@ const BLOCK_DECLARATIONS = [
 
 const BLOCK_DECLARATION_STARTS = BLOCK_DECLARATIONS.map(name => `${name}Start`);
 
+const BLOCK_EXPRESSIONS = [
+  'FunctionExpression',
+  'ClassEexpression',
+];
+
+const BLOCK_EXPRESSION_STARTS = BLOCK_EXPRESSIONS.map(name => `${name}Start`);
+
+const DECLARATIONS = [
+  'VariableDeclaration',
+];
+
 const COMMENTS = [
   'Block',
   'Line',
@@ -24,10 +35,43 @@ const findBeforeFirst = (target, tokens) => {
 };
 
 
+const isBlock = node => {
+  if (BLOCK_DECLARATIONS.includes(node.type)) {
+    return true;
+  }
+  /*
+   * Also treat as block level node:
+   * 
+   *     var foo = function () {};
+   *
+   */
+  if (node.type === 'VariableDeclaration' && node.declarations.length === 1) {
+    if (BLOCK_EXPRESSIONS.includes(node.declarations[0].init.type)) {
+      return true;
+    }
+  }
+  return false;
+};
+
+
+const isBlockStart = node => {
+  return BLOCK_DECLARATION_STARTS.includes(node.type) || BLOCK_EXPRESSION_STARTS.includes(node.type);
+};
+
+
+const isStatement = node => {
+  return DECLARATIONS.includes(node.type);
+};
+
+
+const isComment = node => {
+  return COMMENTS.includes(node.type);
+};
+
+
 exports.findFirstTokenBeforeBody = (node, context) => {
   if (BLOCK_DECLARATIONS.includes(node.type)) {
     return context.getTokenBefore(node.body);
-    // return findBeforeFirst('Punctuator', context.getTokens(node));
   }
 };
 
@@ -45,16 +89,18 @@ exports.blockStartNode = cursorLine => {
 
 exports.ruleFor = info => {
   let rule = 'maxone';
-  if (BLOCK_DECLARATION_STARTS.includes(info.prev.type)) {
+  if (isBlockStart(info.prev)) {
     rule = 'maxone';
   } else if (info.level === 0) {
-    if (BLOCK_DECLARATIONS.includes(info.prev.type)) {
+    if (isBlock(info.prev)) {
       rule = 'two';
+    } else if (isStatement(info.prev) && isStatement(info.current)) {
+      rule = 'maxtwo';
     } else {
       rule = (info.prev.type === 'ProgramStart') ? 'maxtwo' : 'two';
     }
   } else {
-    if (BLOCK_DECLARATIONS.includes(info.prev.type)) {
+    if (isBlock(info.prev)) {
       rule = 'one';
     } else {
       rule = 'maxone';
@@ -62,7 +108,7 @@ exports.ruleFor = info => {
   }
 
   if (rule.indexOf('max') === -1) {
-    if (COMMENTS.includes(info.prev.type)) {
+    if (isComment(info.prev)) {
       rule = `zero${rule}`;
     }
   }
