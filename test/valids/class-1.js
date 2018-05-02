14fae4a3abd3816b3e87f2a2e8c0f55289d05a68
othree <othree@gmail.com>
2018-05-02 18:21:35 +0800

    First simple class test cases

---
+++ test/valids/class-1.js
@@ -0,0 +1,23 @@
+
+
+class A {
+    constructor () {}
+    methodA () {}
+}
+
+
+function func () {
+    var a = 1;
+
+    var b = 1;
+}
+
+
+// Comment Line
+
+
+/* 
+ * comments
+ */
+class B {
+}
