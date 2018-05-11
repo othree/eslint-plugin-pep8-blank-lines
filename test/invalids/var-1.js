1b8203ea9b498ca3b681b14578c4a56e761b19f3
othree <othree@gmail.com>
2018-05-11 08:56:19 +0800

    Add var test case

---
+++ test/invalids/var-1.js
@@ -0,0 +1,11 @@
+
+
+var a = 1;
+
+
+var b = 1;
+// line
+
+
+class B {
+}
---
+++ test/valids/var-1.js
@@ -0,0 +1,17 @@
+
+
+var a = 1;
+var b = 1;
+var c = function () {
+};
+
+
+class A {}
+
+
+// Comment Line
+/* 
+ * comments
+ */
+class B {
+}
