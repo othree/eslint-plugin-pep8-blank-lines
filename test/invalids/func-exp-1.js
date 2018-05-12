7511d5d382566a67d933d0798d80def4c53694b2
othree <othree@gmail.com>
2018-05-12 15:51:06 +0800

    Add func exp tests

---
+++ test/invalids/func-exp-1.js
@@ -0,0 +1,13 @@
+
+
+
+var a = 1;
+var b = 1;
+
+
+var c = function () {
+  let a = 2;
+
+  let b = 2;
+};
+
---
+++ test/valids/func-exp-1.js
@@ -0,0 +1,22 @@
+
+
+var a = 1;
+var b = 1;
+
+
+var c = function () {
+  let a = 2;
+
+  let b = 2;
+};
+
+
+class A {}
+
+
+// Comment Line
+/* 
+ * comments
+ */
+class B {
+}
