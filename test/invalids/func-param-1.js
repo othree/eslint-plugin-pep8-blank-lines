a8eb16a50d2af2d55d4a34336cfc2cf5fdf29879
othree <othree@gmail.com>
2018-05-13 00:14:53 +0800

    Add func-param-1 test case

---
+++ test/invalids/func-param-1.js
@@ -0,0 +1,15 @@
+var a = 1;
+var b = 1;
+
+
+var c = function (
+  // Comment
+
+  a, b) {
+  let e = 2;
+
+  let f = 2;
+};
+
+
+
---
+++ test/valids/func-param-1.js
@@ -0,0 +1,24 @@
+
+
+var a = 1;
+var b = 1;
+
+
+var c = function (
+  // Comment
+  a, b) {
+  let e = 2;
+
+  let f = 2;
+};
+
+
+class A {}
+
+
+// Comment Line
+/* 
+ * comments
+ */
+class B {
+}
