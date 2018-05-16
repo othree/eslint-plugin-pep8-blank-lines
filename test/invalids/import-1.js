c8fad4732777bcb029172ddcc443fcca72135536
othree <othree@gmail.com>
2018-05-16 15:14:25 +0800

    Add import test case

---
+++ test/invalids/import-1.js
@@ -0,0 +1,15 @@
+import a from 'a';
+import b from 'b';
+
+import c from 'c';
+
+var c = function (
+  // Comment
+  a, b) {
+  let e = 2;
+
+  let f = 2;
+};
+
+
+
---
+++ test/valids/import-1.js
@@ -0,0 +1,27 @@
+import a from 'a';
+import b from 'b';
+
+import c from 'c';
+
+
+var c = function (
+  // Comment
+  a, b) {
+  let e = 2;
+
+  let f = 2;
+};
+
+
+class A {}
+
+
+// Comment Line
+/* 
+ * comments
+ */
+class B {
+}
+
+
+import d from 'd';
