c163d505ea2c1684897102c81e387b909ab63e06
othree <othree@gmail.com>
2018-05-16 15:22:18 +0800

    Add export test

---
+++ test/invalids/export-1.js
@@ -0,0 +1,17 @@
+import a from 'a';
+import b from 'b';
+
+import c from 'c';
+
+
+var c = function (
+  // Comment
+  a, b) {
+  let e = 2;
+
+  let f = 2;
+};
+
+
+
+export { c };
---
+++ test/valids/export-1.js
@@ -0,0 +1,31 @@
+import a from 'a';
+import b from 'b';
+
+import c from 'c';
+
+
+var c = function (
+  // Comment
+  a, b) {
+  let e = 2;
+
+  let f = 2;
+};
+
+
+class A {}
+
+
+// Comment Line
+/* 
+ * comments
+ */
+class B {
+}
+
+
+import d from 'd';
+
+
+export { A };
+export { B };
