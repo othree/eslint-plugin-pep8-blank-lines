7d5052456aee44befc670a02f7f6d4aaa5661b80
othree <othree@gmail.com>
2018-05-18 11:17:41 +0800

    Add if test

---
+++ test/valids/if-1.js
@@ -0,0 +1,41 @@
+
+
+if (a) {
+  a = 1;
+} else {
+  a = 2;
+}
+
+
+class A {
+    propA = 1;
+    propB = arg => {
+      var z = arg;
+
+      z + 1;
+    };
+    propC = function () {
+      var z = arg;
+      z + 1;
+    };
+
+    constructor () {}
+    methodA () {}
+}
+
+
+function func () {
+    var a = 1;
+
+    var b = 1;
+}
+
+
+// Comment Line
+
+
+/* 
+ * comments
+ */
+class B {
+}
