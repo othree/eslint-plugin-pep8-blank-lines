bae392dcca55ac16dd2567fd79bd06da53bb4a36
othree <othree@gmail.com>
2018-05-25 00:10:52 +0800

    Add inline and exp

---
+++ test/valids/inline-1.js
@@ -0,0 +1,39 @@
+
+
+var a = 1 +
+        // comment
+        2;
+
+
+class A {
+    propA = 1;
+    propB = arg => {
+      var z = arg;
+
+      z + 1;
+    };
+    propC = function () {
+      var z = arg;
+      z + 1;
+    };
+
+    constructor () {}
+    methodA () {}
+}
+
+
+function func () {
+    var a = 1;
+
+    var b = 1;
+}
+
+
+// Comment Line
+
+
+/* 
+ * comments
+ */
+class B {
+}
