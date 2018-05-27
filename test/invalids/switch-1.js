a8dbf2a3448446122f52db774e2cc1a5dd618a0a
othree <othree@gmail.com>
2018-05-27 12:08:02 +0800

    Add switch test

---
+++ test/invalids/switch-1.js
@@ -0,0 +1,14 @@
+
+
+switch (a) {
+  case 1:
+    b = 1;
+  case  // comment
+
+    2:
+    b = 2;
+  default:
+    b = 3;
+}
+
+
---
+++ test/valids/switch-1.js
@@ -0,0 +1,45 @@
+
+
+switch (a) {
+  case 1:
+    b = 1;
+  case  // comment
+    2:
+    b = 2;
+  default:
+    b = 3;
+}
+
+
+class A {
+    propA = 1;
+    propB = arg => {
+      var z = arg;
+
+      z + 1;
+    };
+    propC = function () {
+      var z = arg;
+      z + 1;
+    };
+
+    constructor () {}
+    methodA () {}
+}
+
+
+function func () {
+    var a = 1;
+
+    var b = 1;
+}
+
+
+// Comment Line
+
+
+/* 
+ * comments
+ */
+class B {
+}
