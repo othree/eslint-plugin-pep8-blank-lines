80756bb1d83d4fbbe9accb16169d791616840fad
othree <othree@gmail.com>
2018-09-11 10:20:32 +0800

    Add case: comment after one statement block

---
+++ test/valids/paran-1.js
@@ -0,0 +1,9 @@
+if (attr === '0') {
+  // Do nothing
+  return text;
+// } else if (attr === '2') {
+//   // smarten ``backticks'' -style quotes
+//   do_backticks = 1;
+// } else {
+//   do_backticks = 0;
+}
